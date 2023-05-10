9c6aa2d7e497c2f6587cfedfdc7676d5d3d17951
MoranSin <nurixhbh@gmail.com>
2023-05-10 20:42:47 +0300

    formalil

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
 $(document).ready(function(){
     $("#nextButton").click(function(){
         debugger
-        // $("#part1").addClass("hide");
         $("#part1").css("display", "none");
-
-        // $("#part2").removeClass("hide");
         $("#part2").css("display" ,"block");
-
-
-        // event.PreventDefualt();
     });
 
     $("#backButton").click(function(){
-        // $("#part2").addClass("hide");
         $("#part2").css("display", "none");
-
-        // $("#part1").removeClass("hide");
         $("#part1").css("display", "block");
-
-        // event.PreventDefualt();
     });
 });
 
