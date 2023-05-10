71ba7fc4ea60cac3014a0bb4ddcd92b7de3781a8
MoranSin <nurixhbh@gmail.com>
2023-05-10 20:15:13 +0300

    admatsijquary

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
     $("#nextButton").click(function(){
         debugger
-        $("#part1").addClass("hide");
-        $("#part2").removeClass("hide");
+        // $("#part1").addClass("hide");
+        $("#part1").css("display", "none");
+
+        // $("#part2").removeClass("hide");
+        $("#part2").css("display" ,"block");
+
+
         // event.PreventDefualt();
     });
 
     $("#backButton").click(function(){
-        $("#part2").addClass("hide");
-        $("#part1").removeClass("hide");
+        // $("#part2").addClass("hide");
+        $("#part2").css("display", "none");
+
+        // $("#part1").removeClass("hide");
+        $("#part1").css("display", "block");
+
         // event.PreventDefualt();
     });
 });
