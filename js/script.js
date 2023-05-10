8a18feaa1ce59c01fce4180751ee7020f361662d
MoranSin <nurixhbh@gmail.com>
2023-05-10 19:59:54 +0300

    jsthings

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
     $("#nextButton").click(function(){
         debugger
-        $("#part1").addClass("hide").removeClass("show");
-        $("#part2").removeClass("hide").addClass("show");
+        $("#part1").addClass("hide");
+        $("#part2").removeClass("hide");
+        // event.PreventDefualt();
     });
 
     $("#backButton").click(function(){
-        $("#part2").addClass("hide").removeClass("show");
-        $("#part1").removeClass("hide").addClass("show");
+        $("#part2").addClass("hide");
+        $("#part1").removeClass("hide");
+        // event.PreventDefualt();
     });
 });
 
