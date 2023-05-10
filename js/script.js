059f35f22370716186168073e2e465696f0ec28c
Adi Levi <adile0194@gmail.com>
2023-05-10 20:42:48 +0300

    search

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
-    $("#nextButton").click(function(){
+$(document).ready(function () {
+    $("#nextButton").click(function () {
         debugger
         $("#part1").addClass("hide").removeClass("show");
         $("#part2").removeClass("hide").addClass("show");
     });
 
-    $("#backButton").click(function(){
+    $("#backButton").click(function () {
         $("#part2").addClass("hide").removeClass("show");
         $("#part1").removeClass("hide").addClass("show");
+    });
+
+    $(".srch").click(function () {
+        $(this).css("display", "none");
+        $(".form-control").css("display", "block");
     });
 });
 
