8896c79a9c0537f680f202b84f37df4cd89f512e
MoranSin <nurixhbh@gmail.com>
2023-07-17 01:15:22 +0300

    kaka al categories

--- protesting.php
+++ protesting.php
@@ -2,8 +2,6 @@
 include 'db.php';
 include 'config.php';
 session_start();
-static $prodId = 6;
-$prodId++; 
 $uid = $_SESSION["user_id"];
 $title = $_POST["proTitle"];
 $summary = $_POST["proSum"];
@@ -14,16 +12,21 @@ if(empty($_POST["allowArt"])){
     $allowArt = 1;
 }
 
-$quary = "INSERT INTO tbl_212_protest (prot_id,prot_title, prot_summary, prot_story, allow_art) VALUES ('$prodId','$title', '$summary' , '$story' , '$allowArt');";
+$quary = "INSERT INTO tbl_212_protest (prot_title, prot_summary, prot_story, allow_art) VALUES ('$title', '$summary' , '$story' , '$allowArt');";
 $prot = mysqli_query($connection,$quary) or die('Quary prot is failed'.mysqli_error($connection));
-$quaryprot = "INSERT INTO tbl_212_prot_user(prot_id, user_id) VALUES ('$prodId','$uid');";
+$getidq = "SELECT prot_id FROM tbl_212_protest ORDER BY prot_id desc limit 1;";
+$protid = mysqli_query($connection,$getidq) or die('Quary protid is failed'.mysqli_error($connection));
+$idrow = mysqli_fetch_assoc($protid);
+$idrow2 = $idrow['prot_id'];
+$pid = strval($idrow2);
+$quaryprot = "INSERT INTO tbl_212_prot_user(prot_id, user_id) VALUES ('$pid','$uid');";
 $protuser = mysqli_query($connection,$quaryprot) or die('Quary protuser is failed'.mysqli_error($connection));
 
 if($prot == 1){
     $pcategories = $_POST["proCate"];
     $pcategory = explode(',', $pcategories);
     foreach ($pcategory as $pcat) {
-        $quary2 = "INSERT IGNORE INTO tbl_212_categories(cat_name) SELECT '$pcat' FROM tbl_212_categories WHERE not exists ( SELECT * FROM tbl_212_categories WHERE cat_name ='$pcat');" ;
+        $quary2 = "INSERT INTO tbl_212_categories(cat_name) SELECT '$pcat' FROM tbl_212_categories WHERE not exists ( SELECT * FROM tbl_212_categories WHERE cat_name ='$pcat');" ;
         if(!$quary2 == 0) {
 
             $cat = mysqli_query($connection,$quary2) or die('Quary cat is failed'. mysqli_error($connection));
@@ -34,10 +37,10 @@ if($prot == 1){
             $row = mysqli_fetch_assoc($catid);
             $cid = $row['cat_id'];
             $result2 =  strval($cid);
-            $quary3 = "INSERT INTO tbl_212_prot_cat(prot_id,cat_id) values ('$prodId','$result2');";
+            $quary3 = "INSERT INTO tbl_212_prot_cat(prot_id,cat_id) values ('$pid','$result2');";
             $protcat = mysqli_query($connection,$quary3) or die('Quary protcat is failed'. mysqli_error($connection));
         }
     }
 }
-header("Location:protest.php?protId=$prodId");
+header("Location:protest.php?protId=$pid");
 ?>
