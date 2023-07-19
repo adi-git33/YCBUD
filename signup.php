ac5b7856e842dedbad7451c1905fe9fad0816ef7
MoranSin <nurixhbh@gmail.com>
2023-07-19 21:07:45 +0300

    artistsignup

--- signup.php
+++ signup.php
@@ -20,6 +20,17 @@ if($role == 'yes'){
 $quary = "INSERT INTO tbl_212_users(name, email, password, user_type) VALUES ('$fullname','$email','$pass','$role');";
 $result = mysqli_query($connection, $quary) or die('Quary signup is failed' . mysqli_error($connection));
 
+if($role = 'artist'){
+    $getida = "SELECT user_id FROM tbl_212_users ORDER BY user_id desc limit 1;";
+    $result2 = mysqli_query($connection, $getida) or die('Quary signup artist is failed' . mysqli_error($connection));
+    $idrow = mysqli_fetch_assoc($result2);
+    $idrow2 = $idrow['user_id'];
+    $aid = strval($idrow2);
+    $artist = "INSERT INTO tbl_212_artist(artist_id) VALUES ('$aid')";
+    $result2 = mysqli_query($connection, $artist) or die('Quary signup artist2 is failed' . mysqli_error($connection));
+
+}
+
 header('Location:login.php');
 
 
